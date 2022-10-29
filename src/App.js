bdae29b3aebf7a8c87b7b22dd4175d21c625e196
mohamed <mohamedbagabas1996@gmail.com>
2022-10-30 01:05:35 +0300

    Homework

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
+import Nav from './Components/Nav';
+import Footer from './Components/Footer';
+import SocialProfileWithImage from './Components/Course';
+import { CalendarIcon } from "@chakra-ui/icons";
 
-function App() {
+function App () {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav/>
+      <div className ="course">
+      <SocialProfileWithImage name = "Campain JS " Course="JS" startdate="12/12/2022" enddate="10/10/2023" date ="1"/>
+      <SocialProfileWithImage name = "Campain Python " Course="Python" startdate="5/6/2020" enddate="10/10/2020" date="13" />
+      <SocialProfileWithImage name = "Campain C# " Course="C#" startdate="6/7/2023" enddate="19/5/2023" date="9" />
+      <SocialProfileWithImage name = "Campain C " Course="C" startdate="25/9/2024" enddate="24/10/2024"  date="5"/>
+      <SocialProfileWithImage name = "Campain Java " Course="Java" startdate="22/7/2023" enddate="10/10/2024"  date="5"/>
+      <SocialProfileWithImage name = "Campain C++ " Course="C++" startdate="12/12/2020" enddate="10/10/2021" date="20" />
+      </div>
+      <footer>
+        <Footer/>
+      </footer>
     </div>
   );
 }
---
+++ src/Components/Course.js
@@ -0,0 +1,99 @@
+
+import {
+    Heading,
+    Avatar,
+    Box,
+    Center,
+    Image,
+    Flex,
+    Text,
+    Stack,
+    Button,
+    useColorModeValue,
+  } from '@chakra-ui/react';
+  import img1 from "../Components/Course.jpg"
+  import img2 from "../Components/Tuwiq.jpg"
+
+  
+  export default function SocialProfileWithImage(props) {
+    
+    let Stats ;
+    let color ;
+    let today = new Date();
+    let todaydate = today.getDate(Date.now);
+    if(props.date < 10 ) {
+        Stats = "Course is open";
+        color = "blue";
+    }
+    else{
+     Stats = "Course is close";
+     color = "black";
+    }
+    return (
+      <Center py={6}>
+        <Box
+          maxW={'270px'}
+          w={'full'}
+          bg={useColorModeValue('white', 'gray.800')}
+          boxShadow={'2xl'}
+          rounded={'md'}
+          overflow={'hidden'}>
+          <Image
+            h={'120px'}
+            w={'full'}
+            src={img1}
+            
+            objectFit={'cover'}
+          />
+          <Flex justify={'center'} mt={-12}>
+            <Avatar
+              size={'xl'}
+              src={img2}
+              
+              alt={'Author'}
+              css={{
+                border: '2px solid white',
+              }}
+            />
+          </Flex>
+  
+          <Box p={6}>
+            <Stack spacing={0} align={'center'} mb={5}>
+              <Heading fontSize={'2xl'} fontWeight={500} fontFamily={'body'}>
+                {props.name}
+              </Heading>
+              <Text color={'gray.500'}>{props.Course}</Text>
+            </Stack>
+  
+            <Stack direction={'row'} justify={'center'} spacing={6}>
+              <Stack spacing={0} align={'center'}>
+                <Text fontWeight={600}>{props.startdate}</Text>
+                <Text fontSize={'sm'} color={'gray.500'}>
+                  Start
+                </Text>
+              </Stack>
+              <Stack spacing={0} align={'center'}>
+                <Text fontWeight={600}>{props.enddate}</Text>
+                <Text fontSize={'sm'} color={'gray.500'}>
+                 Ends
+                </Text>
+              </Stack>
+            </Stack>
+  
+            <Button
+              w={'full'}
+              mt={8}
+              bg={useColorModeValue(color)}
+              color={'white'}
+              rounded={'md'}
+              _hover={{
+                transform: 'translateY(-2px)',
+                boxShadow: 'lg',
+              }}>
+              {(Stats)}
+            </Button>
+          </Box>
+        </Box>
+      </Center>
+    );
+  }
