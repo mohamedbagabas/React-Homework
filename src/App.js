8eca5506761fd43a8953e30242a6064d44edf17b
mohamed <mohamedbagabas1996@gmail.com>
2022-11-01 12:27:18 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,27 +1,78 @@
-import React, { Component } from "react";
+import React, { Component , useState } from "react";
 import './App.css';
 import Nav from './Components/Nav';
 import Footer from './Components/Footer';
 import SocialProfileWithImage from './Components/Course';
 import { CalendarIcon } from "@chakra-ui/icons";
 import FilterSearch from "./Components/Searchbar";
-// import Login from "./Components/Login_page";
+import Login from "./Components/Login_page";
+import LoginForm from "./Components/Login_page";
 
 function App () {
+  const adminUser = {
+    name: "Abdulkarim",
+    password: "123",
+  };
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+  });
+
+  const [error, setError] = useState("");
+
+  const login = (details) => {
+    console.log(details);
+
+    if (
+      details.name === adminUser.name &&
+      details.password === adminUser.password
+    ) {
+      setUser({
+        name: details.name,
+        password: details.password,
+      });
+    } else {
+      console.log("Details Do not Match");
+      <div className="error"> {setError("Details Do not Match!!")}; </div>;
+    }
+    if (details.name === "" || details.password === "") {
+      setError("All fields are required!!");
+    }
+  };
+
+  const Logout = () => {
+    setUser({ name: "", paassword: "" });
+  };
+
   return (
+    
     
     <div className="App">
       
       <Nav/>
-      <FilterSearch/>
+      
+      {user.name !== "" ? (
+        <div className="login-form">
+          <h2>
+            Welcome
+            <span>
+              {user.name} <br />{" "}
+              <span className="success"> You are logged in Successfully! </span>
+            </span>
+          </h2>
+          <button onClick={Logout}> Logout </button>
+        </div>
+      ) : (
+        <LoginForm login={login} error={error} />
+      )}
       <div className ="course">
       
-      <SocialProfileWithImage name = "Campain JS " Course="JS" startdate={("2023-10-12")} enddate={("2024-5-10")} />
+      {/* <SocialProfileWithImage name = "Campain JS " Course="JS" startdate={("2023-10-12")} enddate={("2024-5-10")} />
       <SocialProfileWithImage name = "Campain Python " Course="Python" startdate={"2022,6,12"} enddate={'2023,10,9'}/>
       <SocialProfileWithImage name = "Campain C# " Course="C#" startdate={"2023"} enddate={"2024"}  />
       <SocialProfileWithImage name = "Campain C " Course="C" sta  startdate={2023} enddate={2024}  />
       <SocialProfileWithImage name = "Campain Java " Course="Java" startdate={2023} enddate={2024}/>
-      <SocialProfileWithImage name = "Campain C++ " Course="C++" startdate={("2020")} enddate={(2021)}/> 
+      <SocialProfileWithImage name = "Campain C++ " Course="C++" startdate={("2020")} enddate={(2021)}/>  */}
       </div>
      
       <footer>
